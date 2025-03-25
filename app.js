a7b877caa553caaff420ae85410762dbf3b520a8
Roy Y Liao <yunzhi.liao@gmail.com>
2025-03-25 20:44:29 +1100

    eks initial success

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const port = 3000; // You can use any port you prefer
+
+app.get('/hello', (req, res) => {
+  res.send('Hello!');
+});
+
+app.get('/path', (req, res) => {
+  res.send('Hello from /path!');
+});
+
+app.get('/', (req, res) => {
+    res.send('Hello world!');
+});
+
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
+});
